ab65f9912ec12b32f868ee0d0313665bc9e603e5
luzdelgado <luzdivdelg@gmail.com>
2021-07-20 18:45:02 +0200

    Initial commit & package with tests class created

---
+++ src/test/java/com/example/EventTest.java
@@ -0,0 +1,109 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EventTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testEvent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEventLongStringEventTypeEventNotificationService() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddAttendee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddAttendees() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveAttendee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveAttendees() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotifyAssistants() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddSpeaker() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveSpeaker() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSpeakers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSpeakers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAttendees() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAttendees() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/example/service/EventNotificationServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.example.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EventNotificationServiceImplTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testAnnounce() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConfirmAttendance() {
+		fail("Not yet implemented");
+	}
+
+}
